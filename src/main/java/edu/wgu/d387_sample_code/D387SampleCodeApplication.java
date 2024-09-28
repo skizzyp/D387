1ed548f7a77962490aaf965703b14c6ba3a54fde
Scott Gary <scottmgary@gmail.com>
2024-09-28 11:05:31 -0500

    create racing threads for welcome messages

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -1,15 +1,52 @@
 package edu.wgu.d387_sample_code;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
+	private final List<String> messages = new ArrayList<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
+		Thread englishThread = new Thread(() -> displayWelcomeMessage("welcome_en_US.properties"));
+		Thread frenchThread = new Thread(() -> displayWelcomeMessage("welcome_fr_CA.properties"));
+
+		englishThread.start();
+		frenchThread.start();
+
+		try {
+			englishThread.join();
+			frenchThread.join();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+	private static void displayWelcomeMessage(String propFile) {
+		Properties prop = new Properties();
+		try {
+			InputStream stream = new ClassPathResource(propFile).getInputStream();
+			prop.load(stream);
+			String welcome = prop.getProperty("welcome");
+			System.out.println(welcome);
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 
 }
---
+++ src/main/java/edu/wgu/d387_sample_code/rest/WelcomeController.java
@@ -0,0 +1,13 @@
+package edu.wgu.d387_sample_code.rest;
+
+import edu.wgu.d387_sample_code.D387SampleCodeApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WelcomeController {
+
+}
