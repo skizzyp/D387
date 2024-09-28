708146f1364d9b4993a34f434f3082a589e68d0d
Scott Gary <scottmgary@gmail.com>
2024-09-28 11:26:12 -0500

    implemented threading

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -18,6 +18,32 @@ import static java.util.concurrent.Executors.newFixedThreadPool;
 public class D387SampleCodeApplication {
 	static ExecutorService messageExecutor = newFixedThreadPool(5);
 
+	public static void main(String[] args) {
+		SpringApplication.run(D387SampleCodeApplication.class, args);
+		Properties prop = new Properties();
+		messageExecutor.execute(() -> {
+			try {
+				InputStream stream = new ClassPathResource("welcome_en_US.properties").getInputStream();
+				prop.load(stream);
+				String welcome = prop.getProperty("welcome");
+				System.out.println(welcome);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+
+		messageExecutor.execute(() -> {
+			try {
+				InputStream stream = new ClassPathResource("welcome_fr_CA.properties").getInputStream();
+				prop.load(stream);
+				String welcome = prop.getProperty("welcome");
+				System.out.println(welcome);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
 }
 
 
